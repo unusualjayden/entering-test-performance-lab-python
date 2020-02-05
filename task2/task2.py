aba21485d98be44adaceaa3c8e410d54238ee0a6
Karriem Gimadeev <listentowoes@gmail.com>
2020-02-05 22:27:10 +0300

    task 2 style upd

--- task2/task2.py
+++ task2/task2.py
@@ -3,12 +3,12 @@ import sys
 
 
 def get_distance_between_points(a, b):
-    return m.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2) 
+    return m.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
 
 
 def get_height_of_triangle(a, b, c):
-    ab = get_distance_between_points(a, b) 
-    bc = get_distance_between_points(b, c) 
+    ab = get_distance_between_points(a, b)
+    bc = get_distance_between_points(b, c)
     ac = get_distance_between_points(a, c)
     p = (ab + bc + ac) / 2
     return (2 * m.sqrt(p * (p - ab) * (p - bc) * (p - ac))) / bc
@@ -23,15 +23,14 @@ def parse_file(path):
                    .replace('line', '"line"')
                    .replace("{[", "([")
                    .replace("]}", "])")
-                   .replace(" ", "")
-                   )
+                   .replace(" ", ""))
     return eval(tmp)
 
 
 def find_intersection(c, a, b, r):
     aq = (b[0] - a[0])**2 + (b[1] - a[1])**2 + (b[2] - a[2])**2
     cq = (c[0] - a[0])**2 + (c[1] - a[1])**2 + (c[2] - a[2])**2 - r**2
-    bq = -2*((b[0] - a[0]) * (c[0] - a[0]) + 
+    bq = -2*((b[0] - a[0]) * (c[0] - a[0]) +
              (b[1] - a[1]) * (c[1] - a[1]) +
              (b[2] - a[2]) * (c[2] - a[2]))
     dq = bq**2 - 4*aq*cq
@@ -42,7 +41,7 @@ def find_intersection(c, a, b, r):
         return [a[0] + (b[0] - a[0]) * t,
                 a[1] + (b[1] - a[1]) * t,
                 a[2] + (b[2] - a[2]) * t]
-    else: 
+    else:
         t1 = (-bq + m.sqrt(dq)) / (2 * aq)
         t2 = (-bq - m.sqrt(dq)) / (2 * aq)
         return ([a[0] + (b[0] - a[0]) * t1,
@@ -52,8 +51,7 @@ def find_intersection(c, a, b, r):
                  a[1] + (b[1] - a[1]) * t2,
                  a[2] + (b[2] - a[2]) * t2]
                 )
-    
-        
+
 
 def main():
     figures = parse_file(sys.argv[1])
@@ -61,16 +59,15 @@ def main():
                             figures["line"][0],
                             figures["line"][1],
                             figures["sphere"]["radius"])
-    
-    
-    if res == None:
+
+    if res is None:
         print("Коллизий не найдено")
     elif len(res) == 2:
-        print(res[0]) 
+        print(res[0])
         print(res[1])
     else:
-        print(res) 
-        
+        print(res)
+
 
 if __name__ == "__main__":
     main()
